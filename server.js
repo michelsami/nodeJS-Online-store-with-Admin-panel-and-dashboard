777d457f8916eb7334e8b45ec0d0ad2c60cc6dc9
Mohamed Abdelrahman <moabulrahman.do@gmail.com>
2023-08-01 00:15:11 +0300

    M

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { router as userRouter } from "./routes/authRouter.js";
 import { activityRouter } from "./routes/activity-router.js";
 import { connectionDB } from "./config/database-connection.js";
+import {googleRouter} from './routes/googleAuth.js'
 
 dotenv.config();
 
@@ -12,11 +13,12 @@ const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/activity", activityRouter);
 
 app.use("/users", userRouter);
-
+app.use("/" , googleRouter)
 app.listen(port, () => {
   console.log(`listining to localHost port ${port}`);
 });
