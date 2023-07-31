6f8c08a4218a06a621543be498caf544a162eeee
Mohamed Abdelrahman <moabulrahman.do@gmail.com>
2023-07-31 22:26:53 +0300

    M

--- models/activity-model.js
+++ models/activity-model.js
@@ -2,15 +2,6 @@ import mongoose from "mongoose";
 
 
 const activitySchema = mongoose.Schema({
-	googleId: {
-		type: String,
-		required: false,
-		unique: true
-	},
-	displayName: {
-		type: String,
-		required: false
-	},
 	title:{
 		type: String,
 		trim: true,
